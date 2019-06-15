7c481ad593be5aa40d2dc8c3b9e09654ef7e1c1e
TheChanRProject <garpchoo@gmail.com>
2019-06-14 23:05:57 -0700

    Adding edits

--- git-priv.py
+++ git-priv.py
@@ -5,9 +5,9 @@ username = input("Please type in your username: ")
 repository = input("Please type in the repository name you want to clone: ")
 password = getpass.getpass("Please type in your password: ")
 author_name = input("Please put in the repo's author username from whom you want to clone: ")
-url = f"https://{username}:{password}@github.com/{author_name}/{repository}.git"
-
+url1 = f"https://{username}:{password}@github.com/{author_name}/{repository}.git"
+url2 = f"https://{username}:{password}@github.com/{username}/{repository}.git"
 if username == author_name:
-    os.system(f"git clone {f"https://{username}:{password}@github.com/{username}/{repository}.git"}")
+    os.system(f"git clone {url2}")
 else:
-    os.system(f"git clone {url}") 
+    os.system(f"git clone {url1}") 
