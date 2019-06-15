a6773e76a4e4a15ff18e16926e81c304cec62bec
TheChanRProject <garpchoo@gmail.com>
2019-06-14 22:53:32 -0700

    Adding edits

--- git-priv.py
+++ git-priv.py
@@ -4,7 +4,7 @@ import getpass
 username = input("Please type in your username: ")
 repository = input("Please type in the repository name you want to clone: ")
 password = getpass.getpass("Please type in your password: ")
-
-url = f"https://{username}:{password}@github.com/{username}/{repository}.git"
+author_name = input("Please put in the repo's author username from whom you want to clone: ")
+url = f"https://{username}:{password}@github.com/{author_name}/{repository}.git"
 
 os.system(f"git clone {url}")
